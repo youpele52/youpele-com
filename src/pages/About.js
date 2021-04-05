d1282e5b08300604e8c65e4f90862e3a5d655276
Youpele Michael <mjayjesus@gmail.com>
2021-04-05 16:40:59 +0200

    About me css updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const About = () => {
   return (
-    <section className='section '>
-      <div className='title'>
-        <h2>About me</h2>
-      </div>
+    <Wrapper>
+      <section className='section  '>
+        <div className='title'>
+          <h2>About me</h2>
+        </div>
 
-      <p>
-        Here is my favorite quote from Steve Jobs, "The people who are crazy
-        enough to think they can change the world are the ones who do." Now you
-        know about me.
-      </p>
-    </section>
+        <p>
+          Here is my favorite quote from Steve Jobs, "The people who are crazy
+          enough to think they can change the world are the ones who do." Now
+          you know about me.
+        </p>
+      </section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  p {
+    text-align: center;
+  }
+`
 
 export default About
