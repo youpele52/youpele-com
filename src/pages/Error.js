e764cb616cc16dfb880b9df6b4267cf5f159ba12
Youpele Michael <mjayjesus@gmail.com>
2021-04-05 15:41:41 +0200

    Refining error page.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,24 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-const Poetry = () => {
+const ErrorPage = () => {
   return (
-    <section className='section '>
+    <section className='section page-100  '>
       <div className='title'>
         <h2>
-          <span role='img' aria-label>
+          404
+          {/* <span role='img' aria-label>
             🥴
-          </span>
+          </span> */}
         </h2>
         <p>
           We are stuck with technology when all we really want is just stuff
           that works. How do you recognize something that is still technology? A
           good clue is if it comes with a manual. ~ Douglas Adams
         </p>
+        <Link to='/' className='btn'>
+          Back home
+        </Link>
       </div>
-
-      {/* <p>Comming soon...</p> */}
     </section>
   )
 }
 
-export default Poetry
+export default ErrorPage
