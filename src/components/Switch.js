33de222f37970e67a21a74723af3ba9034ba4997
Youpele Michael <mjayjesus@gmail.com>
2021-07-07 13:43:27 +0200

    Update Switch.js

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Switch = ({ isOn, handleToggle, onColor }) => {
-  console.log(isOn)
+  // console.log(isOn)
   return (
     <SwitchContainer>
       <input
